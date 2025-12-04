fdb07ee7dc763af42f836578d816a1b62c055a0e
Alexander Kusakin <pm12.kusakin@gmail.com>
2025-12-04 16:56:41 +0700

    kus1

--- lr1/generate_square.py
+++ lr1/generate_square.py
@@ -1,3 +1,4 @@
+import math 
 def print_square_coords(square_size: float, nx: int) -> None:
     h = square_size / (nx - 1)
 
@@ -6,12 +7,12 @@ def print_square_coords(square_size: float, nx: int) -> None:
         row_parts = []
         for ix in range(nx):
             x = ix * h
-            row_parts.append(f"{x:.2f} {y:.2f}")
+            row_parts.append(f"{x:.7f} {y:.7f}")
         print("  ".join(row_parts))
 
 
 if __name__ == "__main__":
-    square_size = 0.4  
-    nx = 5            
+    square_size =   math.sqrt(2)*1e-6
+    nx = 10            
 
     print_square_coords(square_size, nx)
